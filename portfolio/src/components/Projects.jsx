a9058abb958118a898952d9105ec98b5b748600e
Gloria <ubahgloria16@gmail.com>
2023-03-28 12:40:25 +0100

    Chore: Added animation to the project section.

--- portfolio/src/components/Projects.jsx
+++ portfolio/src/components/Projects.jsx
@@ -1,14 +1,30 @@
 import React, {useState, useEffect } from "react";
 import { VscChevronLeft } from "react-icons/vsc";
 import style from "./style.module.css";
-import data from "../data/project";
+import Data from "../data/project";
 
 const Projects = () => {
+     const [displayData, setDisplayData] = useState(Data);
      const [index, setIndex] = useState(0);
+     const data = displayData.filter(info => {
+          return info.id === index ? info : null;
+     });
+     const firstProject = () => {
+          setIndex(firstIndex => firstIndex = 0);
+     }
+     const secondProject = () => {
+          setIndex(secondIndex => secondIndex = 1);
+     }
+     const thirdProject = () => {
+          setIndex(thirdIndex => thirdIndex = 2);
+     }
+     const fourthProject = () => {
+          setIndex(fourthIndex => fourthIndex = 3);
+     }
     
  return (
       <section className={`bg-about-section border-none p-4`}>
-           <div className={`relative`}>
+           <div className={`relative z-10`}>
                     <div className={`relative`}>
                          <h2 className={`text-about-header font-bold text-3xl`}>
                               <span className={`font-bold text-xl`}>
@@ -21,16 +37,16 @@ const Projects = () => {
                                    </span>
                          </h2>
                     </div>
-                {data.map((info) => {
-                        return (<div className={`my-8 px-8 border-l-4 border-light-blue ${info.id === index ? `block` : `hidden`}`} key={info.id}> 
+                {data.map(info => {
+                        return (<div className={`${style.project_div} my-8 px-8 border-l-4 border-light-blue`} key={info.id}> 
                              <h3 className={`font-semibold text-lg text-about-header`}>{info.title}</h3>
                          <p className={`text-body-text text-base font-semibold font-["Nunito"] text-left py-4`}>{info.description}</p>
                          <div className={`flex justify-start items-center`}>
-                              <a href={info.url} className={`border-b-2 border-light-blue text-about-header font-semibold pb-2`}>Visit Site</a>
-                              <a href={info.github} className={`border-b-2 border-light-blue text-about-header font-semibold pb-2 ml-8`}>Github</a>  
+                              <a href={info.url} className={`border-b-2 border-light-blue text-about-header font-semibold pb-2 animate-pulse`}>Visit Site</a>
+                              <a href={info.github} className={`border-b-2 border-light-blue text-about-header font-semibold pb-2 ml-8 animate-pulse`}>Github</a>  
                          </div>
                     </div>)
-                   }) }
+                   })}
                </div>
                
                <div className={`relative my-10 p-5`}>
@@ -38,14 +54,12 @@ const Projects = () => {
                          <VscChevronLeft className={`${style.project_border} w-64 h-64 text-gradient1 rotate-45`} />
                     </div>
                     <div className={`flex flex-col items-start p-4`}>
-                         <div className={`border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4`} >Proj1</div>
-                         <div className={`border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4`}  >Proj2</div>  
-                         <div className={`border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4`} >Proj3</div>
-                     <div className={`border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4`}>Proj4</div>
+                         <button className={`z-10 border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4 transition duration-700 ease-in-out`} onClick={firstProject}>Proj1</button>
+                         <button className={`z-10 border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4 transition duration-700 ease-in-out `} onClick={secondProject}>Proj2</button>  
+                         <button className={`z-10 border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4 transition duration-700 ease-in-out`} onClick={thirdProject}>Proj3</button>
+                     <button className={`z-10 border-b-2 border-light-blue text-about-header font-semibold pb-1 mb-4 transition duration-700 ease-in-out`} onClick={fourthProject}>Proj4</button>
                 </div>
                     </div>
-
-                    
      </section>
 )
 }
--- portfolio/src/data/project.js
+++ portfolio/src/data/project.js
@@ -9,21 +9,21 @@ const Projects = [
      {
           id: 1,
           title: "Proj2",
-          description: "1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur est inventore ipsa ducimus dignissimos harum debitis fugit ipsum voluptatem ut eius earum cupiditate sapiente quae illum unde, recusandae accusamus!",
+          description: "2 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur est inventore ipsa ducimus dignissimos harum debitis fugit ipsum voluptatem ut eius earum cupiditate sapiente quae illum unde, recusandae accusamus!",
           url: "Visit Site",
           github: "Github"
      },
      {
           id: 2,
           title: "Proj3",
-          description: "1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur est inventore ipsa ducimus dignissimos harum debitis fugit ipsum voluptatem ut eius earum cupiditate sapiente quae illum unde, recusandae accusamus!",
+          description: "3 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur est inventore ipsa ducimus dignissimos harum debitis fugit ipsum voluptatem ut eius earum cupiditate sapiente quae illum unde, recusandae accusamus!",
           url: "Visit Site",
           github: "Github"
      },
      {
           id: 3,
           title: "Proj4",
-          description: "1 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur est inventore ipsa ducimus dignissimos harum debitis fugit ipsum voluptatem ut eius earum cupiditate sapiente quae illum unde, recusandae accusamus!",
+          description: "4 Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores consequuntur est inventore ipsa ducimus dignissimos harum debitis fugit ipsum voluptatem ut eius earum cupiditate sapiente quae illum unde, recusandae accusamus!",
           url: "Visit Site",
           github: "Github"
      },
